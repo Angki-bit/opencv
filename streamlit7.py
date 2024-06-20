7d29468e4307ef114b33b16bb29056642e19a1a9
Angki-bit <sispambumen@gmail.com>
2024-06-20 14:43:55 +0700

    Update streamlit7.py

--- streamlit7.py
+++ streamlit7.py
@@ -1,16 +1,1 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-from tracker import *
-import tempfile
 
-
-def main():
-  st.title("PENCACAH LALULINTAS")
- 
-
-if __name__ == '__main__':
-    try:
-        main()
-    except SystemExit:
-        pass
