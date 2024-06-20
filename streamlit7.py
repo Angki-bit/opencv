8c3db154bf48620da3d860f77621d4cd81bd00d0
Angki-bit <sispambumen@gmail.com>
2024-06-20 15:19:15 +0700

    Update streamlit7.py

--- streamlit7.py
+++ streamlit7.py
@@ -4,11 +4,7 @@ from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 def main():
     st.title("PENCACAH LALULINTAS")
-    col1,col2,col3,col4 = st.columns(4)
-    file = 'ban.jpg'
-    classes = ["ban"]
-    img = cv2.imread(str(file))
-    st.image(str(file))
+    st.image('ban.JPG')
 
 if __name__ == '__main__':
     try:
