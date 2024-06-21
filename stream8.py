8e239e3c6d10f10bc9a6f36f99e71fb4ec10d829
Angki-bit <sispambumen@gmail.com>
2024-06-22 06:52:51 +0700

    Update stream8.py

--- stream8.py
+++ stream8.py
@@ -5,13 +5,16 @@ import cv2
 import numpy as np
 from tracker import *
 import pandas as pd
+
 def main():
     st.title("PENCACAH LALULINTAS")
     cap = cv2.VideoCapture('potong.mp4')
     tempat = st.empty()
+    i = 0
     while cap.isOpened():
         ret, frame = cap.read()
-    
+        st.write(str(i))
+        i=i+1
 if __name__ == '__main__':
     try:
         main()
