e63f2fcc5941c328189c470f85585958871adaa8
Angki-bit <sispambumen@gmail.com>
2024-06-22 05:31:28 +0700

    Update streamlit7.py

--- streamlit7.py
+++ streamlit7.py
@@ -9,7 +9,8 @@ def main():
     img = cv2.imread(img1)
     st.image(img,channels='RGB')
     mobil = 'https://sipenjalu.net/assets/mobil.onnx'
-    net = cv2.dnn.readNetFromONNX(mobil)
+    response = requests.get(mobil)
+    net = cv2.dnn.readNetFromONNX(response)
     blob = cv2.dnn.blobFromImage(img, scalefactor=1/255, size=[640, 640], mean=[0, 0, 0], swapRB=True, crop=False)
     net.setInput(blob)
 if __name__ == '__main__':
