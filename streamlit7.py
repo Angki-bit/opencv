f49dec967424442241d13abe5d9fa485bcb06826
Angki-bit <sispambumen@gmail.com>
2024-06-22 05:18:35 +0700

    Update streamlit7.py

--- streamlit7.py
+++ streamlit7.py
@@ -6,14 +6,7 @@ def main():
     st.title("PENCACAH LALULINTAS")
     img1 = 'ban.JPG'
     img = cv2.imread(img1)
-    net = cv2.dnn.readNetFromONNX("mobil.onnx")
-    blob = cv2.dnn.blobFromImage(img, scalefactor=1/255, size=[640, 640], mean=[0, 0, 0], swapRB=True, crop=False)
-    net.setInput(blob)
-    detections = net.forward()[0]
-    classes_ids = []
-    confidences = []
-    boxes = []
-    rows = detections.shape[0]
+    st.image(img)
 
 if __name__ == '__main__':
     try:
