d36ca1c09f4c9d1ca2a78e7f70deb949b33da7d5
Angki-bit <sispambumen@gmail.com>
2024-06-22 05:24:23 +0700

    Update streamlit7.py

--- streamlit7.py
+++ streamlit7.py
@@ -6,7 +6,8 @@ import cv2
 def main():
     st.title("PENCACAH LALULINTAS")
     img1 = 'ban.JPG'
- 
+    img = cv2.imread(img1)
+    st.image(img,channels='RGB')
 if __name__ == '__main__':
     try:
         main()
