4ed06e9a1a8985f739f5379ef2cff3a5abc3b915
Angki-bit <sispambumen@gmail.com>
2024-06-22 06:51:55 +0700

    Update stream8.py

--- stream8.py
+++ stream8.py
@@ -11,7 +11,7 @@ def main():
     tempat = st.empty()
     while cap.isOpened():
         ret, frame = cap.read()
-        tempat.image(frame,channels='RGB')
+    
 if __name__ == '__main__':
     try:
         main()
